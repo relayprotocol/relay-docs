282c57a10710eda7365dbe4b2282141d8ee4daa9
pedromcunha <pedro.spinsomewebs@gmail.com>
2025-12-10 23:29:17 -0500

    Add ms clarity for sessions

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ async function loadPostHog() {
 }
 
 loadPostHog();
+
+//Load ms clarity
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "ujoarm77s7");
